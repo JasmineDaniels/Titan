fa517c98f1cede68b9c3f24e306b60d0e4c0a065
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-09-29 11:38:03 -0400

    bootstrap min js - navigation

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import './css/root.css'
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.css";
 // Bootstrap Bundle JS
-// import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
